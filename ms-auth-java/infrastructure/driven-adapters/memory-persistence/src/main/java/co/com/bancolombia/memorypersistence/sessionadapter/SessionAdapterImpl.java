789569de95871c216c631baa0dfbcda75f3c8c93
bolivero <bolivero@bancolombia.com.co>
2025-10-03 11:30:42 -0500

    Add session management using ConcurrentHashMap for improved session storage

--- ms-auth-java/infrastructure/driven-adapters/memory-persistence/src/main/java/co/com/bancolombia/memorypersistence/sessionadapter/SessionAdapterImpl.java
+++ ms-auth-java/infrastructure/driven-adapters/memory-persistence/src/main/java/co/com/bancolombia/memorypersistence/sessionadapter/SessionAdapterImpl.java
@@ -4,14 +4,15 @@ import co.com.bancolombia.model.session.Session;
 import co.com.bancolombia.model.session.gateways.SessionRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class SessionAdapterImpl  implements SessionRepository {
-
+    private final ConcurrentHashMap<String, String> sessions = new ConcurrentHashMap<>();
 
     @Override
     public Mono<Session> save(Session session) {
-
+        sessions.put(session.getSessionId(), session.getEmail());
         return Mono.just(session);
     }
 }
